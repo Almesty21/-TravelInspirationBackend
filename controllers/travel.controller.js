a44dfc3dfca9016d7da4861c9fcd576415156d39
almesty21 <mihretabgirma21@gmail.com>
2023-10-10 17:41:18 -0300

    commit message

--- controllers/travel.controller.js
+++ controllers/travel.controller.js
@@ -1,10 +1,6 @@
-
-const travelService = require('../services/travel.service');
-const path = require("path");
 const multer = require("multer");
-const GridFsStorage = require("multer-gridfs-storage");
-const Grid = require("gridfs-stream");
-const crypto = require("crypto");
+const path = require("path");
+const travelService = require('../services/travel.service');
 
 // Create a new travel
 exports.createTravel = async (req, res) => {
@@ -50,58 +46,30 @@ exports.deleteTravel = async (req, res) => {
     res.status(500).json({ error: 'Unable to delete Travel' });
   }
 };
-let gfs;
 
-conn.once("open", () => {
-  gfs = Grid(conn.db, mongoose.mongo);
-  gfs.collection("uploads");
-  console.log("Connection Successful");
-});
-
-// Create storage engine
-const storage = new GridFsStorage({
-  url: mongoURI,
-  file: (req, file) => {
-    return new Promise((resolve, reject) => {
-      crypto.randomBytes(16, (err, buf) => {
-        if (err) {
-          return reject(err);
-        }
-        const filename = file.originalname;
-        const fileInfo = {
-          filename: filename,
-          bucketName: "uploads"
-        };
-        resolve(fileInfo);
-      });
-    });
+// Set up Multer storage configuration
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // Set the destination folder where uploaded files will be stored
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    // Set the file name for the uploaded image
+    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
   }
 });
 
-const upload = multer({ storage });
+// Initialize Multer with the storage configuration
+const upload = multer({ storage: storage });
 
-app.post("/", upload.single("img"), (req, res, err) => {
-  res.send(req.files);
-});
+// Handle POST requests to the /upload endpoint
+app.post("/upload", upload.single("image"), (req, res) => {
+  // Multer adds the file object to the request object (req.file)
+  // You can now use req.file to access the uploaded image details
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
 
-app.get("/:filename", (req, res) => {
-  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
-    // Check if file
-    if (!file || file.length === 0) {
-      return res.status(404).json({
-        err: "No file exists"
-      });
-    }
-
-    // Check if image
-    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
-      // Read output to browser
-      const readstream = gfs.createReadStream(file.filename);
-      readstream.pipe(res);
-    } else {
-      res.status(404).json({
-        err: "Not an image"
-      });
-    }
-  });
+  // File uploaded successfully
+  res.send("File uploaded!");
 });
