13ae144e7e673fdd5c6c1c1c0d9ef2d7ecda8739
almesty21 <mihretabgirma21@gmail.com>
2023-10-10 16:10:03 -0300

    commit message

--- services/travel.service.js
+++ services/travel.service.js
@@ -3,23 +3,23 @@ const Travel = require('../models/Travel');
 
 // Create a new travel
 exports.createTravel = async (travelData) => {
-  const travel = new Song(travelData);
+  const travel = new Travel(travelData);
   return await song.save();
 };
 
 // Retrieve all travel
 exports.getAllTravel = async () => {
-  return await Song.find();
+  return await Travel.find();
 };
 
 // Update a travel by ID
 exports.updateTravel = async (travelId, updatedData) => {
-  return await Song.findByIdAndUpdate(travelId, updatedData, { new: true });
+  return await Travel.findByIdAndUpdate(travelId, updatedData, { new: true });
 };
 
 // Delete a travel by ID
 exports.deleteTravel = async (travelId) => {
-  return await Song.findByIdAndDelete(travelId);
+  return await Travel.findByIdAndDelete(travelId);
 };
 
 
