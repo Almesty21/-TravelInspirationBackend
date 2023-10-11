b5316a69838cb2a2b84b180a3c34312293ea437e
Mihretab <48807537+Almesty21@users.noreply.github.com>
2023-10-11 03:33:01 -0300

    Update travel.controller.js

--- controllers/travel.controller.js
+++ controllers/travel.controller.js
@@ -1,5 +1,4 @@
-const multer = require("multer");
-const path = require("path");
+
 const travelService = require('../services/travel.service');
 
 // Create a new travel
@@ -46,30 +45,3 @@ exports.deleteTravel = async (req, res) => {
     res.status(500).json({ error: 'Unable to delete Travel' });
   }
 };
-
-// Set up Multer storage configuration
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    // Set the destination folder where uploaded files will be stored
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    // Set the file name for the uploaded image
-    cb(null, file.fieldname + "-" + Date.now() + path.extname(file.originalname));
-  }
-});
-
-// Initialize Multer with the storage configuration
-const upload = multer({ storage: storage });
-
-// Handle POST requests to the /upload endpoint
-app.post("/upload", upload.single("image"), (req, res) => {
-  // Multer adds the file object to the request object (req.file)
-  // You can now use req.file to access the uploaded image details
-  if (!req.file) {
-    return res.status(400).send("No file uploaded.");
-  }
-
-  // File uploaded successfully
-  res.send("File uploaded!");
-});
